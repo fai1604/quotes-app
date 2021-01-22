2ca0a5c8e445c82877fb0b7d008994b85dd5163a
Mohamad Fairuz <mohamadfairuz1604@gmail.com>
2021-01-22 13:33:06 +0800

    changed title of document

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const authorDisplay = document.querySelector('#author')
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    
     const name = form.elements.query.value;
     const res = await axios.get('https://type.fit/api/quotes')
     const num = Math.floor(Math.random() * 1642)
@@ -13,27 +14,8 @@ form.addEventListener('submit', async function (e) {
     const author = res.data[num].author
     const heading = `Dear ${name}, `
     
-    
     headingDisplay.textContent = heading
     quoteDisplay.textContent = selectedQuote;
     authorDisplay.textContent = author
-    
-      
-
-      
 })
 
-// const options = {
-//   method: 'GET',
-//   url: 'https://quotes15.p.rapidapi.com/quotes/random/',
-//   headers: {
-//     'x-rapidapi-key': '301626cf34mshfd321ccdb79b314p10a602jsnf44a97ccd7b8',
-//     'x-rapidapi-host': 'quotes15.p.rapidapi.com'
-//   }
-// };
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
